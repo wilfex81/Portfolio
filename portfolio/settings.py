0f40686f3d98507177e295dfa7bf1a271348be95
wilfex <wilfex81@gmail.com>
2022-10-17 10:28:03 +0300

    csrf

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%ptk(a7z$#$xibzr7+9k7#d$0s0y97mcg7etwzb@jbewhgpxbs
 DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-405a.up.railway.app']
-#CSRF_TRUSTED_ORIGINS = ['https://web-production-405a.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-405a.up.railway.app']
 
 
 # Application definition
